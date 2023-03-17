e7727dcdcee01fb02b107dba58238943d94af0c2
peng.chen <chen_peng7@163.com>
2023-03-17 22:17:45 +0800

    20230317 add go-ethereum

--- src/gateway/logic/control/block_chain.go
+++ src/gateway/logic/control/block_chain.go
@@ -1,9 +1,14 @@
 package control
 
 import (
+	"context"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/gin-gonic/gin"
+	oCommon "web3_practice/common"
 	"web3_practice/common/consts"
 	ctrl "web3_practice/common/control"
+	"web3_practice/common/exception"
 	"web3_practice/lib/proto/blockchain"
 )
 
@@ -22,6 +27,24 @@ func (c *blockChainCtrl) GetTransactionDetail(ctx *gin.Context) {
 		return
 	}
 
+	// curl https://eth-mainnet.g.alchemy.com/v2/L1GdDpXQwe_eqs6QI4ewxwEdROCklTza -H 'Content-Type: application/json' -X POST --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}'
+	client, err := ethclient.Dial("https://eth-mainnet.g.alchemy.com/v2/L1GdDpXQwe_eqs6QI4ewxwEdROCklTza")
+	if err != nil {
+		oCommon.Logger.Warningf("GetTransactionDetail BalanceAt fail. | err: %s", err)
+		ctrl.Exception(ctx, exception.New(exception.CodeInternalError))
+		return
+	}
+
+	account := common.HexToAddress("0x71c7656ec7ab88b098defb751b7401b5f6d8976f")
+	balance, err := client.BalanceAt(context.Background(), account, nil)
+	if err != nil {
+		oCommon.Logger.Warningf("GetTransactionDetail BalanceAt fail. | err: %s", err)
+		ctrl.Exception(ctx, exception.New(exception.CodeInternalError))
+		return
+	}
+
+	oCommon.Logger.Infof("balance: %s", balance)
+
 	rsp := &blockchain.GetTransactionRsp{
 		Code:    consts.RespCodeSuccess,
 		Message: consts.RespMsgSuccess,
@@ -29,7 +52,7 @@ func (c *blockChainCtrl) GetTransactionDetail(ctx *gin.Context) {
 			Tx:       "aaa",
 			From:     "bbb",
 			To:       "ccc",
-			Amount:   "0.1154",
+			Amount:   balance.String(),
 			GasFee:   "0.001564548",
 			GasPrice: "0.0000024564",
 		},
